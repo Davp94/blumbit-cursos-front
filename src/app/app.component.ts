a98a19f0e76b9e7f8183f03039bcc67f8703bf2b
davp94 <david_villca@devoruro.com>
2024-04-23 07:44:36 -0400

    add initial project settings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { ButtonModule } from 'primeng/button';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, ButtonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'cursos-online-front';
+  greeting = 'HOLA MUNDO';
+  greeting2 = '';
 }
